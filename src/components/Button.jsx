3c115f272c3f93c9d274817fb71077705a038ce8
Eitan GV <eitan9994@gmail.com>
2024-03-21 16:51:44 +0200

    commants added to Button.jsx

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,5 +1,14 @@
+// Import React for component creation
 import React from "react";
-function Button(props){
-    return (<button className="button">{props.text}</button>);}
-export default Button;
 
+// Define the Button functional component with props
+function Button(props){
+    // Return JSX for the component
+    return (
+        // Render a button element with a class and display the text passed via props
+        <button className="button">{props.text}</button>
+    );
+}
+
+// Export the Button component for use in other parts of the application
+export default Button;
