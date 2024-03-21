01bd4d9e4d9c6af3d8b2f5c94719e86ea6f5bcc9
Eitan GV <eitan9994@gmail.com>
2024-03-21 17:34:42 +0200

    commants added to AddCalories.jsx

--- src/components/AddCalories.jsx
+++ src/components/AddCalories.jsx
@@ -1,14 +1,18 @@
+// Import React, useState hook, and custom components
 import React, { useState } from "react";
 import Button from "./Button";
 import AddMealTextFields from "./AddMealTextFields";
 
+// Define the AddCalories functional component with destructured props
 function AddCalories({isLoading, db = {}, rows, setRows}) {
+    // Initialize state for mealData with default empty values
     const [mealData, setMealData] = useState({
         calories: "",
         description: "",
         category: "",
     });
 
+    // Define an asynchronous event handler for form submission
     const handleAddCalories = async (event) => {
          // Prevent default form submission behavior
         try {
@@ -17,21 +21,22 @@ function AddCalories({isLoading, db = {}, rows, setRows}) {
             db.addCalories(mealData);
             // Update the table data
             db.readCalories().then((r) => setRows(r));
-            event.preventDefault();
-            // Update mealData state to clear the form fields
+            event.preventDefault();    // Prevent default form submission behavior
+            // Reset mealData state to clear the form fields
             setMealData({
                 calories: "",
                 description: "",
                 category: "",
             });
-            console.log("Meal added successfully!");
+            console.log("Meal added successfully!"); // Success message
         } catch (error) {
-            console.error("Error adding meal:", error);
+            console.error("Error adding meal:", error); // Error message
         }
     };
 
-
+    // Render the form with text fields and a submit button
     return (
+        // Render inside a div h1 title, text field for meal data, and submit button
         <div>
             <form className="add-calories-container" onSubmit={handleAddCalories}>
                 <h1>Add your meal:</h1>
@@ -42,4 +47,5 @@ function AddCalories({isLoading, db = {}, rows, setRows}) {
     );
 }
 
+// Export the AddCalories component for use in other parts of the application
 export default AddCalories;
