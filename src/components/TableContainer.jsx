995e26cee4faf53f28753129b29ae66717f80cc5
Eitan GV <eitan9994@gmail.com>
2024-03-21 12:38:26 +0200

    comments added TableContainer.jsx

--- src/components/TableContainer.jsx
+++ src/components/TableContainer.jsx
@@ -1,10 +1,12 @@
+// Import necessary modules from React and components from local files
 import React from "react";
 import Table from "./Table";
 import ReportBtns from "./ReportBtns";
 
+// Define the TableContainer functional component with props
 function TableContainer({isLoading, db = {}, rows, setRows})
 {
-
+    // Return JSX for the component
     return(
         <div className={"table-container"}>
             <ReportBtns />
@@ -13,4 +15,5 @@ function TableContainer({isLoading, db = {}, rows, setRows})
     );
 }
 
+// Export the TableContainer component for use in other parts of the application
 export default TableContainer;
