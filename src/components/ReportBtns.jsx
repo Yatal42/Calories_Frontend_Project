ad738c2ce084137c0970c0cbe79dd7ce8c20d29a
Eitan GV <eitan9994@gmail.com>
2024-03-21 15:46:00 +0200

    commands added to ReportBtns.jsx

--- src/components/ReportBtns.jsx
+++ src/components/ReportBtns.jsx
@@ -1,13 +1,16 @@
+// Import React and the DatePickers component
 import React from "react";
 import DatePickers from "./DatePickers";
 
-
+// Define the ReportBtns functional component
 function ReportBtns(){
-
+    // Return JSX for the component
     return (
+        // Use a div with a class to style the container
         <div className="report-container">
             <DatePickers />
         </div>);
 }
 
+// Export the ReportBtns component for use in other parts of the application
 export default ReportBtns;
