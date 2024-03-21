74418ff0ab7003717725325acf879b5b1b2c7d64
Eitan GV <eitan9994@gmail.com>
2024-03-21 15:53:12 +0200

    commants added to Heading.jsx

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -1,10 +1,14 @@
+// Import React for component creation
 import React from "react";
 
+// Define the Heading functional component
 function Heading() {
+  // Get the current date and time
   const date = new Date();
   const currentTime = date.getHours();
-  let greeting="My calories ";
+  let greeting="My calories "; // Initialize the greeting message
 
+  // Determine the greeting based on the current time
   if (currentTime < 12 && currentTime > 5 ) {
     greeting += "Good morning";
   } else if (currentTime < 20 && currentTime>16) {
@@ -13,15 +17,17 @@ function Heading() {
     greeting += "Good Night";
   }
 
+  // Return JSX for the component
   return (
+      // Use a div with a class to style the container
       <div className="heading-container">
         <span className="material-symbols-rounded">dinner_dining</span>
         <h1 className="heading">
           {greeting}
         </h1>
       </div>
-
   );
 }
 
+// Export the Heading component for use in other parts of the application
 export default Heading;
